bac615bf81d8420ecd28d9129edc0883ce30492a
nehak6296 <nehak6296@gmail.com>
2021-03-23 14:11:06 +0530

    [Neha]Added: Removing first node element

--- LinkedList/LinkedList/LinkedList.cs
+++ LinkedList/LinkedList/LinkedList.cs
@@ -69,6 +69,13 @@ namespace LinkedList
             return head;
             
         }
+        internal Node RemoveFirstNode()
+        {
+            if (this.head == null)
+                return null;
+            this.head = this.head.next;
+            return this.head;
+        }
 
     }
 }
--- LinkedList/LinkedList/Program.cs
+++ LinkedList/LinkedList/Program.cs
@@ -11,6 +11,7 @@ namespace LinkedList
             list.Add(56);            
             list.Add(70);
             list.InsertAtParticularPosition(2, 30);
+            list.RemoveFirstNode();
             list.Display();
         }
     }
