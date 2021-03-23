07956a936a554b245b327a708409acfb3fcbaa29
nehak6296 <nehak6296@gmail.com>
2021-03-23 13:13:58 +0530

    [Neha]Add: Linked list of 56,30,70

--- LinkedList/LinkedList/Program.cs
+++ LinkedList/LinkedList/Program.cs
@@ -7,6 +7,11 @@ namespace LinkedList
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Linked List Demo..");
+            LinkedList list = new LinkedList();
+            list.Add(56);
+            list.Add(30);
+            list.Add(70);
+            list.Display();
         }
     }
 }
