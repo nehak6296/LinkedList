20c8563c760370accba481b1fc4e0b3b70d4f3b8
nehak6296 <nehak6296@gmail.com>
2021-03-23 13:19:47 +0530

    [Neha]Add: Linked list by adding 30 & 56 to70

--- LinkedList/LinkedList/Program.cs
+++ LinkedList/LinkedList/Program.cs
@@ -8,9 +8,9 @@ namespace LinkedList
         {
             Console.WriteLine("Welcome to Linked List Demo..");
             LinkedList list = new LinkedList();
-            list.Add(56);
-            list.Add(30);
             list.Add(70);
+            list.Add(30);
+            list.Add(56);
             list.Display();
         }
     }
